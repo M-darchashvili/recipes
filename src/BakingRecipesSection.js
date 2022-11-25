b065a509099ff00c6e826d5f9a9227e003bba66e
Pitt Khotchada <brobot454@gmail.com>
2022-11-25 13:01:20 +0100

    Start work on project

---
+++ src/BakingRecipesSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BakingRecipesSection() {
+  return (
+    <section>
+      <header className="section-header">
+        <h1>Baking Recipes</h1>
+      </header>
+    </section>
+  );
+}
+
+export default BakingRecipesSection;
---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <header className="hero">
+      <h1>Pitt's Favorite Recipes</h1>
+    </header>
+  );
+}
+
+export default Hero;
